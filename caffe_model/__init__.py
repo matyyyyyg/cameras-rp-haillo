0168914be9e9d3a3a3f90b26cf84981deb3d7836
matyasdf <matyas.prokopius@datafriends.cz>
2026-02-24 18:02:19 +0100

    Cleaning v2

--- caffe_model/__init__.py
+++
@@ -1,2 +0,0 @@
-from .classifier import LightweightClassifier, ClassificationResult, extract_face_crop
-from .pipeline import SimplePipeline
--- insightface_model/__init__.py
+++
@@ -1,2 +0,0 @@
-from .classifier import InsightFaceClassifier, ClassificationResult, extract_face_crop
-from .pipeline import SimplePipeline
